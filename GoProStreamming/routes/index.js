49b63760b71b8963cc3e99b8ff42906f01448fa2
Luan Andrade <luansandrade92@hotmail.com>
2015-09-06 04:52:16 -0300

    Arquivos
    lê e apaga arquivo. passa comandos.

--- GoProStreamming/routes/index.js
+++ GoProStreamming/routes/index.js
@@ -4,44 +4,29 @@ var router = express.Router();
 var Camera = require('gopro').Camera;
 var cam = new Camera('10.5.5.9', 'goprohero');
 var fs = require('fs');
-var com = require("serialport");
-
-
+var FILE = "public/arquivo.txt";
 
 /* GET home page. */
 router.get('/', function(req, res) {
 
 
-    //io.on('connection', function(socket) {
-    //    socket.emit('gopro', {hello: 'world'});
-    //});
-
-  
-    /*cam.directory(function(data, err){
-        dirData = fs.readdirSync(directoryData);
-        console.log(dirData);
-        console.log('jh');
-    });*/
-
-	
-	var serialPort = new com.SerialPort("PORTA", {
-	    baudrate: 9600,
-	    parser: com.parsers.readline('\r\n')
-	  });
-
-	serialPort.on('open',function() {
-	  console.log('Port open');
-	});
-
-	serialPort.on('data', function(data) {
+	fs.readFile(FILE, 'utf8', function (err,data) {
+	  if (err) {
+	    return console.log(err);
+	  }
+	  
 	  console.log(data);
-	  if(data=='start')
-	  	cam.startCapture();
-	  else if(data=='stop')
-	  	cam.stopCapture();
+		if(data=='start')
+			cam.startCapture();
+		else if(data=='stop')
+			cam.stopCapture();
+
 	});
-
-
+	
+	fs.unlink(FILE, function (err) {
+  	if (err) throw err;
+  		console.log('Não foi possível deletar arquivo.');
+	});
 	
 	
 
