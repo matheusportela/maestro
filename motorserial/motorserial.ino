2d0bd463fb3a02b1e59b266c073e03c78a04e586
Matheus Portela <matheus.v.portela@gmail.com>
2015-09-05 21:31:19 -0300

    Movimentos posicionam a câmera.

--- motorserial/motorserial.ino
+++ motorserial/motorserial.ino
@@ -17,42 +17,117 @@
 #define DOUBLE_TAP_COMMAND 5
 #define UNKNOWN_COMMAND 6
 
-Servo servo1; // horizontal (bottom) motor
-Servo servo2; // vertical (upper) motor
- 
-int pos = 0;    // variable to store the servo position 
+#define SERVO_1_PIN 9  // horizontal (bottom) motor [0, 180]
+#define SERVO_1_MIN 0
+#define SERVO_1_MAX 180
+#define SERVO_1_STEP 20
+
+#define SERVO_2_PIN 10 // vertical (upper) motor [0, 30]
+#define SERVO_2_MIN 0
+#define SERVO_2_MAX 30
+#define SERVO_2_STEP 10
+
+Servo servo1;
+Servo servo2;
+
+int controlled_motor = 1;
+int servo_1_deg = 0;
+int servo_2_deg = 0;
  
 void setup() 
 { 
-  servo1.attach(9);  // attaches the servo on pin 9 to the servo object 
-//  servo2.attach(10);  // attaches the servo on pin 9 to the servo object 
   Serial.begin(9600);
+  
+  servo1.attach(SERVO_1_PIN);
+  servo2.attach(SERVO_2_PIN);
+  
+  moveMotor1(0);
+  moveMotor2(0);
 } 
- 
+
+void moveMotor1(int deg)
+{
+  servo_1_deg += deg;
+  
+  if (servo_1_deg < SERVO_1_MIN)
+    servo_1_deg = SERVO_1_MIN;
+  else if (servo_1_deg > SERVO_1_MAX)
+    servo_1_deg = SERVO_1_MAX;
+  
+  servo1.write(servo_1_deg);
+}
+
+void moveMotor2(int deg)
+{
+  servo_2_deg += deg;
+  
+  if (servo_2_deg < SERVO_2_MIN)
+    servo_2_deg = SERVO_2_MIN;
+  else if (servo_2_deg > SERVO_2_MAX)
+    servo_2_deg = SERVO_2_MAX;
+  
+  servo2.write(servo_2_deg);
+}
+
 void loop() 
 { 
   if (Serial.available()) {
-    int pos = Serial.read();
+    int command = Serial.read();
     
-    switch (pos)
+    switch (command)
     {
+      case REST_COMMAND:
+        break;
+      
       case FIST_COMMAND:
         Serial.println("Fist");
+        
+        if (controlled_motor == 1)
+        {
+          controlled_motor = 2;
+          Serial.println("Controlled motor: 2");
+        }
+        else if (controlled_motor == 2)
+        {
+          controlled_motor = 1;
+          Serial.println("Controlled motor: 1");
+        }
+        
         break;
       
       case WAVE_IN_COMMAND:
         Serial.println("Wave in");
-        servo1.write(0);
+        
+        if (controlled_motor == 1)
+          moveMotor1(-SERVO_1_STEP);
+        else if (controlled_motor == 2)
+          moveMotor2(-SERVO_2_STEP);
+          
         break;
         
       case WAVE_OUT_COMMAND:
         Serial.println("Wave out");
-        servo1.write(90);
+
+        if (controlled_motor == 1)
+          moveMotor1(SERVO_2_STEP);
+        else if (controlled_motor == 2)
+          moveMotor2(SERVO_2_STEP);
+
+        break;
+      
+      case FINGERS_SPREAD_COMMAND:
+        break;
+        
+      case DOUBLE_TAP_COMMAND:
+        break; 
+       
+      case UNKNOWN_COMMAND:
+        Serial.write("Unknown command");
         break;
       
       default:
         Serial.write("Received other: ");
-        Serial.println(pos);
+        Serial.println(command);
         break;
     }
   }
